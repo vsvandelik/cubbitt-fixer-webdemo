6af9b53d31e5e468a9ab476fc8a96d1b6c3a5576
Vojtěch Švandelík <vojtech.svandelik@gmail.com>
2021-04-14 01:01:35 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const { src, dest, watch, series } = require('gulp');
+const concat = require('gulp-concat');
+
+function scripts() {
+    return src(['node_modules/jquery/dist/jquery.js','./static/js/develop.js'])
+        .pipe(concat('main.js'))
+        .pipe(dest('./static/js/'));
+}
+
+function styles() {
+    return src(['node_modules/bootstrap/dist/css/bootstrap.css','./static/css/develop.css'])
+        .pipe(concat('main.css'))
+        .pipe(dest('./static/css/'));
+}
+
+function watchAssets(){
+    watch(['static/js/develop.js'], scripts);
+    watch(['static/css/develop.css'], styles);
+}
+
+exports.scripts = scripts;
+exports.styles = styles;
+
+exports.watch = series(scripts, styles, watchAssets);
