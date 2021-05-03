7071d14d81b632e743ecc39f67293e52ed02fb9f
Vojtěch Švandelík <vojtech.svandelik@gmail.com>
2021-05-03 22:52:43 +0200

    adapted for the new fixer configurator

--- cubbittfixer.py
+++ cubbittfixer.py
@@ -1,8 +1,7 @@
 import os
 
-from flask import Flask, render_template, send_from_directory, request, jsonify
-
-from fixer import Fixer
+from fixer import Fixer, FixerConfigurator
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
@@ -14,21 +13,17 @@ def main():
 
 @app.route('/fix', methods=['POST'])
 def fix():
-    src_lang = request.args.get('src', 'cs', type=str)
-    trg_lang = request.args.get('trg', 'en', type=str)
+    default_configuration = get_default_configuration()
+    if 'src' in request.args.keys():
+        default_configuration.source_lang = FixerConfigurator.get_language(request.args, 'src')
+
+    if 'trg' in request.args.keys():
+        default_configuration.source_lang = FixerConfigurator.get_language(request.args, 'trg')
 
     src_text = request.form.get('source_text', '', type=str)
     trg_text = request.form.get('target_text', '', type=str)
 
-    class Arguments:
-
-        def __init__(self, source_lang: str, target_lang: str):
-            self.recalculate = False
-            self.approximately = True
-            self.source_lang = source_lang
-            self.target_lang = target_lang
-
-    fixer = Fixer(Arguments(src_lang, trg_lang))
+    fixer = Fixer(default_configuration)
     translation, _ = fixer.fix(src_text, trg_text)
 
     # there was no fix or the sentence is unfixable
@@ -41,3 +36,23 @@ def fix():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+def get_default_configuration():
+    default_config = {
+        'source_lang': 'cs',
+        'target_lang': 'en',
+        'aligner': 'fast_align',
+        'lemmatizator': 'udpipe',
+        'names_tagger': 'nametag',
+        'mode': 'recalculating',
+        'base_tolerance': 0.1,
+        'approximately_tolerance': 0.1,
+        'target_units': ['Imperial', 'USD', 'F'],
+        'exchange_rates': 'cnb'
+    }
+
+    configuration = FixerConfigurator()
+    configuration.load_from_dict(default_config)
+
+    return configuration
